ac734cb54cde09d0ff38020db8b95fa2cb73ec9f
Hacker Hippo <37157257+hacker-hippo@users.noreply.github.com>
2023-04-16 09:53:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const correctPassPhrase = 'rylee';
+const correctPassPhrase = 'yourpassphrase';
 
 function checkPassPhrase() {
-    const inputText = document.getElementById('input-text').value;
-    const inputPassPhrase = document.getElementById('input-passphrase').value;
+  const inputText = document.getElementById('input-text').value;
+  const inputPassPhrase = document.getElementById('input-passphrase').value;
 
-    if (inputPassPhrase === correctPassPhrase) {
-        document.getElementById('main-text').innerText = inputText;
-    } else {
-        alert('Incorrect pass phrase. Please try again.');
-    }
+  if (inputPassPhrase === correctPassPhrase) {
+    dbRef.update({ mainText: inputText });
+  } else {
+    alert('Incorrect pass phrase. Please try again.');
+  }
 
-    // Reset the form input fields
-    document.getElementById('phrase-form').reset();
+  // Reset the form input fields
+  document.getElementById('phrase-form').reset();
 
-    // Prevent page refresh on form submission
-    return false;
+  // Prevent page refresh on form submission
+  return false;
 }
+
+// Initialize a listener for changes in the mainText value in the database
+dbRef.child('mainText').on('value', (snapshot) => {
+  const mainText = snapshot.val();
+  if (mainText) {
+    document.getElementById('main-text').innerText = mainText;
+  }
+});
