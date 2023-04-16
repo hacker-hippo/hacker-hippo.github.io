c08d0a9eb2c34875002cae2e55a84435a3c8798d
Hacker Hippo <37157257+hacker-hippo@users.noreply.github.com>
2023-04-16 17:02:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+
 const correctPassPhrase = 'rylee';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY6wdUvL50VGw4B7FfDVIDITSfQWOgBeM",
+  authDomain: "githubpages-21eff.firebaseapp.com",
+  projectId: "githubpages-21eff",
+  storageBucket: "githubpages-21eff.appspot.com",
+  messagingSenderId: "1056284598072",
+  appId: "1:1056284598072:web:79673a81805cd181056bcd",
+  measurementId: "G-VLBD0L9SBW"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const dbRef = firebase.database().ref();
 
 function checkPassPhrase() {
   const inputText = document.getElementById('input-text').value;
